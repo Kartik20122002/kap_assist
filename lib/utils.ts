6b497b309a99c81d834667e2be939ee4ff5d3566
kartik <kartikhatwar98@gmail.com>
2026-06-21 14:36:00 +0530

    done

--- lib/utils.ts
+++ lib/utils.ts
@@ -55,10 +55,10 @@ export const getNextTaskStatuses = (currentName: string) => {
 
 export const US_FLOW = [
   { id: 1, name: "New" },
-  { id: 7, name: "Assigned" },
-  { id: 8, name: "Ready" },
+  // { id: 7, name: "Assigned" },
+  // { id: 8, name: "Ready" },
   { id: 2, name: "In Progress" },
-  { id: 9, name: "Implemented" },
+  // { id: 9, name: "Implemented" },
   { id: 5, name: "Closed" }
 ]
 
